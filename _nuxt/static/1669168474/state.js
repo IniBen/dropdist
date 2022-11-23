7e6e4e1bd34a77c1ed9b3aa5d7f40ed2507690e7
iniben <93434228+IniBen@users.noreply.github.com>
2022-11-23 06:18:05 +0100

    re-work
    netlify regeneration

---
+++ _nuxt/static/1669168474/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1669168474",layout:"indexLayout",error:a,state:{products:void 0},serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null,"\u002F"));
--- plugins/commerce.js
+++
@@ -1,4 +0,0 @@
-import Commerce from '@chec/commerce.js'
-import Vue from 'vue'
-
-Vue.prototype.$commerce = new Commerce(process.env.COMMERCEJS_API_KEY, true)
